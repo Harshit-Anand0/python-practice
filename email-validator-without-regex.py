ba1e2b1220c519f7620facd7d96170410a52984f
Harshit-Anand0 <harshitt9931@gmail.com>
2026-08-09 04:51:51 +0530

    Updates code

--- email-validator-without-regex.py
+++ email-validator-without-regex.py
@@ -1,70 +1,96 @@
-#EMAIL VALIDATION PROGRAM(without regex)
-while True:
-    user = input('Enter a valid email: ')
+user = input('Enter your Email address: ')
+try:
+# Program breaker
+    def find_error(value):
+        if not value:
+            raise ValueError('Email is not valid.')
+        return value
 
-    #checking empty,numeric start,spaces,double dot,@ and @ count
-    if (len(user) == 0 
-        or ' ' in user 
-        or '..' in user 
-        or '@' not in user 
-        or user.count('@') != 1):
-        print('Invalid email 1')
-        continue
-    
-    #checking allowed char  
-    allowed = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890._%+-@')
-    invalid = False
+# Part 1 basic validation of whole email address.
+    def basic_validation(Email=""):
 
-    for char in user:
-        if char not in allowed:
-            print('Invalid email 2>',char)
-            invalid = True
-            break
+        allowed_char = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&*-+=/^_`}{|~?@.')
+
+        for ch in Email:                                                                                                    
+            if ch not in allowed_char:
+                return False
+
+        if (len(Email.split('@')) > 2 or
+            Email == "" or
+            " " in Email or
+            ".." in Email or
+            "@" not in Email or
+            "." not in Email or
+            "@@" in Email):
+            return False
+
+        return Email
+
+    user = basic_validation(user)
+    print('run1',user)
+    find_error(user)
+
+# Part 2 complete validation of local part.
+    def local_part_validation(Email):
+
+        email = Email.split("@")[0]
+
+        if (not email or
+            email.startswith(".")  or
+            email.endswith(".")):
+            return False 
     
-    if invalid:
-        invalid = False
-        continue
+        return Email
+
+    user = local_part_validation(user)
+    print('run2',user)
+    find_error(user)
+
+# Part 3 complete validation of subdomain names.
+    def subdomain_and_domain_validation(labels):
+
+        if labels.split("@")[1].startswith('.'):
+            return False
+
+        labels = labels.split("@")[1].split(".")
+
+        for ch in labels:
+            if (ch.startswith("-") or
+                ch.endswith("-") or
+                "." in ch or
+                "_" in ch or
+                len(ch) > 64):
+                return False
+    
+        return labels 
+
+    user = subdomain_and_domain_validation(user)
+    print('run3',user)
+    find_error(user)
         
-    #validating local part
-    local_name = user.split('@')
+# Part 4 complete validation of domain name.
+    def top_level_domain_validation(tld):
 
-    if (local_name[0].startswith('.') 
-        or local_name[0] == '' 
-        or local_name[0].endswith('.')):
-        print('Invalid local name 1')
-        continue 
-        
+        tld = tld[-1]
 
-    #validating domain name and top level domain name
-    domain_name = local_name[1].split('.')
-
-    if len(domain_name[0]) < 1:
-        print('Invalid domain name 1')
-        continue
-
-    allowed_domain = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-')
-
+        if (not tld or
+            len(tld) < 2 or
+            not tld.isalpha()):
+            return False
     
+        return tld
 
-    for el in domain_name:
-        for ch in el:
-            if (ch not in allowed_domain 
-                or el.startswith('-')
-                or el.endswith('-')):
-                print('Invalid domain name or top level domain name 1')
-                invalid = True
-                break
-    if invalid:
-        invalid = False
-        continue
+    user = top_level_domain_validation(user)
+    print('run4',user)
+    find_error(user)
 
-    if len(domain_name[-1]) < 2:
-        print('Invalid top level domain name 1')
-        continue
-           
-    #printing  final result
-    print('Your correct email is: ',user)
+    print("Your Email address is Valid.")
     break
+
+except ValueError as _:
+    print(_)
+
+
     
     
 
